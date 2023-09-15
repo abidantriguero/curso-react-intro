61ed9580941f8fae3fa4bb8cb0b2f878334daa78
Abidan Triguero <abidantriguero@gmail.com>
2023-09-15 15:53:53 -0400

    CLASE 9: Buscando TODOs

--- src/App.js
+++ src/App.js
@@ -20,6 +20,12 @@ function App() {
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
 
+  const searchedTodos = todos.filter((todo) => {
+    const todoText = todo.text.toLowerCase();
+    const searchText = searchValue.toLowerCase();
+    return todo.text.toLowerCase().includes(searchValue.toLocaleLowerCase())
+  });
+
   console.log('Los usuarios buscan todos de ' + searchValue);
   return (
     <>
@@ -30,7 +36,7 @@ function App() {
       />
 
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
